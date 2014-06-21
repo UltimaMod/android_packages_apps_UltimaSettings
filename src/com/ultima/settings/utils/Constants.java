2fe708fb477ff97fed89cf7e1bb56963b659a7fe
Kryten2k35 <kryten2k35@gmail.com>
2014-06-21 15:51:48 +0100

    Revamped the boot animation installer to include CWM flashable zips, adn added an open recovery script generator to enable easy installing. I also moved all hard coded strings out of the boot animation classes so they can be translated properly later.
    Conflicts:
    	res/values/strings.xml
    	src/com/ultima/settings/utils/Constants.java

--- src/com/ultima/settings/utils/Constants.java
+++ src/com/ultima/settings/utils/Constants.java
@@ -1,6 +1,7 @@
 package com.ultima.settings.utils;
 
 import android.content.Context;
+import android.os.Environment;
 
 import com.ultima.settings.SettingsApplication;
 
@@ -8,6 +9,7 @@ public interface Constants {
 	
 	public static final String PREF_NAME = "RagnarSettings";
 	public static Context CONTEXT = SettingsApplication.getContext();
+	public static String SDCARD = Environment.getExternalStorageDirectory().getPath();
 
 	//Theme
 	public static final String CURRENT_THEME = "current_theme";
@@ -18,8 +20,10 @@ public interface Constants {
 	public static final String ORIGNAL_HOSTNAME = "original_hostname";
 	
 	//Boot Animation Chooser
-	public static final String IS_BOOT_ENABLED = "is_boot_enabled";
-	public static final String REBOOT_AFTER_SELECTION = "reboot_after_selection";
-	public static final int REQUEST_CODE = 6384; // onActivityResult request code
+	public static final String BOOTANI_IS_BOOT_ENABLED = "bootani_is_boot_enabled";
+	public static final String BOOTANI_REBOOT_AFTER_SELECTION = "bootani_reboot_after_selection";
+	public static final String BOOTANI_USE_ORS = "bootani_use_open_recovery_script";
+	public static final String BOOTANI_WIPE_CACHE = "bootani_wipe_cache";
+	public static final int BOOTANI_REQUEST_CODE = 6384;
 
 }
